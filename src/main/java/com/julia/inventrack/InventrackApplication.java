02ad2666a33712886bb0fae71ae83b4996119795
Julia dos Santos <santos.ju1995@gmail.com>
2025-06-16 23:14:49 -0300

    first commit

---
+++ src/main/java/com/julia/inventrack/InventrackApplication.java
@@ -0,0 +1,13 @@
+package com.julia.inventrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/julia/inventrack/InventrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julia.inventrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
